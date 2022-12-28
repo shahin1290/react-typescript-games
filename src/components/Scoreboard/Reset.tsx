b0479efa294983e8bee1b5d0d59daeae893cace2
shahin1290 <s.patowary@yahoo.com>
2022-12-28 11:42:00 +0100

    updates game logic with ts-emotion

---
+++ src/components/Scoreboard/Reset.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from "react";
+import styled from "@emotion/styled";
+
+import { useMouseDown } from "../hooks/useMouseDown";
+
+export interface ResetProps {
+  /**
+   * Reset action handler
+   */
+  onReset: () => void;
+}
+
+export const Reset: FC<ResetProps> = ({ onReset }) => {
+  const [mouseDown, onMouseDown, onMouseUp] = useMouseDown();
+
+  return (
+    <Button
+      onMouseDown={onMouseDown}
+      onMouseLeave={onMouseUp}
+      onMouseUp={onMouseUp}
+      onClick={onReset}
+    >
+      {mouseDown ? "😯" : "🙂"}
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  font-size: 1.5vw;
+  cursor: pointer;
+  font-weight: 700;
+  border-width: 0.15vw;
+  border-style: solid;
+  background-color: #d1d1d1;
+  border-color: white #9e9e9e #9e9e9e white;
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import { Scoreboard } from "./components/Scoreboard";
+import { Top } from "./components/Top";
+
 import "./index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <>
+    <Top feature="Flag" firstAction="ctrl" secondAction="click">
+      Minesweeper
+    </Top>
+    <Scoreboard
+      time="000"
+      levels={["beginner", "intermediate", "expert"]}
+      mines="010"
+      onReset={() => null}
+    />
+  </>
+);
